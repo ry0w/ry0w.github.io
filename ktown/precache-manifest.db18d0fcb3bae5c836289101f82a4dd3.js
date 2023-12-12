f2a180be1e2437a5f89734797825c98977cd3b2c
ry0w <yoppy.gochi@gmail.com>
2023-12-12 23:16:53 +0900

    [add]: ファイル追加

---
+++ ktown/precache-manifest.db18d0fcb3bae5c836289101f82a4dd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93e3879a5e0ba2ec5dd5",
+    "url": "/static/css/main.a4004a71.chunk.css"
+  },
+  {
+    "revision": "93e3879a5e0ba2ec5dd5",
+    "url": "/static/js/main.93e3879a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3eb4790f4814411be28a1f7ec253a6d3",
+    "url": "/index.html"
+  }
+];
